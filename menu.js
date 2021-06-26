db71bdffc12c66251243cb65e28e86bf0cddf75b
trexrush <edmazuera@gmail.com>
2021-06-26 17:00:41 -0500

    copied menu
    grabbing current state of menu from my sandbox repo
    also cleanup and some css

--- menu.js
+++ menu.js
@@ -1,3 +1,193 @@
-let test = document.createElement("P")
-test.textContent = "child test"
-document.body.appendChild(test)
+import * as THREE from "https://threejs.org/build/three.module.js";
+
+import { TrackballControls } from 'https://threejs.org/examples/jsm/controls/TrackballControls.js';
+import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+
+import { CSS3DRenderer, CSS3DObject } from 'https://threejs.org/examples/jsm/renderers/CSS3DRenderer.js';
+import { TWEEN } from 'https://threejs.org/examples/jsm/libs/tween.module.min.js';
+import Stats from 'https://threejs.org/examples/jsm/libs/stats.module.js';
+import { GUI } from 'https://threejs.org/examples/jsm/libs/dat.gui.module.js';
+
+import { OBJLoader } from 'https://threejs.org/examples/jsm/loaders/OBJLoader.js';
+import { EffectComposer } from 'https://threejs.org/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://threejs.org/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'https://threejs.org/examples/jsm/postprocessing/ShaderPass.js';
+import { OutlinePass } from 'https://threejs.org/examples/jsm/postprocessing/OutlinePass.js';
+import { FXAAShader } from 'https://threejs.org/examples/jsm/shaders/FXAAShader.js';
+
+// for page transitions use window.history
+
+let scene,
+    sceneCSS, 
+    camera,
+    rendererWEBGL,
+    rendererCSS3D,
+    controls,
+    holding;
+const cssScale = 50 // scaling factor for html/css elements
+
+
+init();
+// OBJECTS //
+
+    // webgl //
+const geometry = new THREE.BoxGeometry(2, 2, 2); // CONTROL CUBE
+const material = new THREE.MeshBasicMaterial( { color: 0xbfc908 } ); //4d4d4d grey //bfc908 yellow
+const cube = new THREE.Mesh( geometry, material );
+scene.add(cube);
+
+    // css3d //
+
+// wrap in function pls
+let labelF = document.createElement('div') // content same, name is labelS
+labelF.classList.add('html', 'label') // content same, name is labelS
+labelF.innerHTML = 'Front' // content varies, name is labelS
+const f = new CSS3DObject( labelF ) // name is s, content uses variable of name labelS
+f.scale.set(-1,1,1) // content same, name is s
+sceneCSS.add(f); // content uses variable of name s
+
+let labelR = document.createElement('div')
+labelR.classList.add('html', 'label')
+labelR.innerHTML = 'Right'
+const r = new CSS3DObject( labelR )
+r.scale.set(-1,1,1)
+sceneCSS.add(r);
+
+let labelU = document.createElement('div')
+labelU.classList.add('html', 'label')
+labelU.innerHTML = 'Upper'
+const u = new CSS3DObject( labelU )
+u.scale.set(-1,1,1)
+sceneCSS.add(u);
+
+let labelB = document.createElement('div')
+labelB.classList.add('html', 'label')
+labelB.innerHTML = 'Back'
+const b = new CSS3DObject( labelB )
+b.scale.set(-1,1,1)
+sceneCSS.add(b);
+
+let labelL = document.createElement('div')
+labelL.classList.add('html', 'label')
+labelL.innerHTML = 'Left'
+const l = new CSS3DObject( labelL )
+l.scale.set(-1,1,1)
+sceneCSS.add(l);
+
+let labelD = document.createElement('div')
+labelD.classList.add('html', 'label')
+labelD.innerHTML = 'Down'
+const d = new CSS3DObject( labelD )
+d.scale.set(-1,1,1)
+sceneCSS.add(d);
+
+// EVENTS //
+let mouseX = 0;
+let mouseY = 0;
+document.addEventListener('mousemove', function(e) {
+    mouseX = ( e.clientX / window.innerWidth ) * 2 - 1;
+    mouseY = - ( e.clientY / window.innerHeight ) * 2 + 1;
+});
+
+window.addEventListener( 'resize', function(e) {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    rendererWEBGL.setSize(window.innerWidth, window.innerHeight);
+    rendererCSS3D.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
+
+// INIT and ANIMATION LOOP //
+
+function init() {
+    // core THREE.js //
+    scene = new THREE.Scene();
+    sceneCSS = new THREE.Scene();
+    sceneCSS.scale.set(1/cssScale, 1/cssScale, 1/cssScale);
+    scene.fog = new THREE.Fog( new THREE.Color( 0x1a1a1a), 0, 10)
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.z = 6;
+
+    rendererWEBGL = new THREE.WebGLRenderer( { alpha: true } );
+    rendererWEBGL.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById('webgl').appendChild( rendererWEBGL.domElement );
+
+    rendererCSS3D = new CSS3DRenderer();
+    rendererCSS3D.setSize( window.innerWidth, window.innerHeight );
+    rendererCSS3D.domElement.style.position = 'absolute';
+    document.getElementById('css3d').appendChild( rendererCSS3D.domElement );    
+    
+    controls = new TrackballControls( camera, rendererCSS3D.domElement );
+    controls.noZoom = true;
+    controls.panSpeed = 5;
+    controls.rotateSpeed = 5;
+    controls.dynamicDampingFactor = 0.1; // MAKE IT SO ONMOUSEDOWN THIS IS MORE, LIKE .1 or .15
+}
+
+function animate() { // animate loop
+    requestAnimationFrame( animate );
+    update();
+    render();
+};
+animate();
+
+function update() {
+    // wrap in function pls
+    affixlabeltext(f, cube, 'f');
+    affixlabeltext(r, cube, 'r');
+    affixlabeltext(u, cube, 'u');
+    affixlabeltext(b, cube, 'b');
+    affixlabeltext(l, cube, 'l');
+    affixlabeltext(d, cube, 'd');
+
+    controls.update();
+}
+
+function render() {
+    rendererWEBGL.render( scene, camera );
+    rendererCSS3D.render( sceneCSS, camera );
+}
+
+// FUNCTIONS //
+
+function resetView() {
+    camera.up = new THREE.Vector3(0,0,1);
+    camera.lookAt(new THREE.Vector3(0,0,0));
+}
+
+function affixlabeltext(labelObj, blockObj, side, offsetunits) { // THIS IMPLIES NO CONTROLS CUZ IT SCREWS IT UP
+    // get midpoint of front face
+    let transformMatrix = blockObj.matrix;
+    let z = blockObj.geometry.parameters.depth * 1.25;
+    let y = blockObj.geometry.parameters.width * 1.25;
+    let x = blockObj.geometry.parameters.height * 1.25;
+    
+    switch(side) {
+        case 'f':
+            labelObj.position.set(0,0,(z) * cssScale);
+            break;
+        case 'b':
+            labelObj.position.set(0,0,(z) * -cssScale);
+            break;
+        case 'u':
+            labelObj.position.set(0,(y) * cssScale,0);
+            break;
+        case 'd':
+            labelObj.position.set(0,(y) * -cssScale,0);
+            break;
+        case 'l':
+            labelObj.position.set((x) * -cssScale,0,0);
+            break;
+        case 'r':
+            labelObj.position.set((x) * cssScale,0,0);
+            break;
+    }
+
+    labelObj.scale.set(1,1,1);
+    labelObj.lookAt(camera.position);
+
+    labelObj.position.applyMatrix4(transformMatrix);
+    labelObj.quaternion.copy(camera.quaternion);
+}
