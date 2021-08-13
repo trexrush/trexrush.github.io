9080d2687928d7228b339573d5d71c0f3e701d48
trexrush <edmazuera@gmail.com>
2021-08-12 20:32:25 -0500

    Updates

--- js/mainpageUI.js
+++
@@ -1,7 +0,0 @@
-let container = document.getElementById('container')
-
-let title = document.createElement('div')
-title.classList.add('title')
-title.innerHTML = "E D U A R D O &emsp; M A Z U E R A"
-
-container.appendChild(title)
